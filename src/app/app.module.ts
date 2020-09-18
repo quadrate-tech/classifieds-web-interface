289eec3c0a2739676396973da323d9553e39c902
Ashvini029 <p.aishuash@gmail.com>
2020-09-18 14:34:56 +0530

    create-component-to-create-district_#1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+
 import { AppComponent } from './app.component';
-import { PromotionAdDetailsComponent } from './promotion-ad-details/promotion-ad-details.component';
-import { PromotionComponent } from './promotion/promotion.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateDistrictComponent } from './create-district/create-district.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PromotionAdDetailsComponent,
-    PromotionComponent
+    CreateDistrictComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
