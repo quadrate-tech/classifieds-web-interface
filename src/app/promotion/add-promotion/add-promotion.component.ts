28525748935416a70e0886c0edc33ea8c091101f
pankajan05 <pankajan05@gmail.com>
2020-09-24 12:14:23 +0530

    create UI and component for the add promotion

--- src/app/promotion/add-promotion/add-promotion.component.ts
+++ src/app/promotion/add-promotion/add-promotion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {PromotionService} from '../promotion.service';
 
 @Component({
   selector: 'app-add-promotion',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-promotion.component.css']
 })
 export class AddPromotionComponent implements OnInit {
+  alert = false;
+  addPromotion = new FormGroup({
+    promotion_id: new FormControl(''),
+    promotion_name: new FormControl(''),
+    promotion_cost: new FormControl(''),
+    duration: new FormControl(''),
+    promotion_status: new FormControl('')
+  });
 
-  constructor() { }
+  constructor(private promotionService: PromotionService) { }
 
   ngOnInit(): void {
+  }
+
+  savePromotion(){
+    this.promotionService.savePromotion(this.addPromotion.value).subscribe((result) => {
+      this.alert = true;
+      this.addPromotion.reset({});
+    });
+  }
+
+  closeAlert(){
+    this.alert = false;
   }
 
 }
