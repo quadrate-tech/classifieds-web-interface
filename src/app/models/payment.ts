0b51e16c92f6da57787c64c78307f41268ed1cc3
Apeksha <apeksha.2018562@iit.ac.lk>
2020-11-16 22:01:45 +0000

    updates

---
+++ src/app/models/payment.ts
@@ -0,0 +1,8 @@
+export class Payment {
+  id: number;
+  promoted_ad_id: number;
+  payment_date: string;
+  payment_time: number;
+  paid_amount: number;
+  payment_status: boolean;
+}
--- src/app/payments/payment.service.ts
+++ src/app/payments/payment.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
+
   providedIn: 'root'
 })
 export class PaymentService {
