53b032d79f28ebf53cfcf17f3cc1a9b120b46b50
shanthu-hammer <55648960+shanthu-hammer@users.noreply.github.com>
2020-11-27 15:46:12 +0800

    type script file

--- src/app/ad/edit-ad-type/edit-ad-type.component.ts
+++ src/app/ad/edit-ad-type/edit-ad-type.component.ts
@@ -1,20 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AdType } from 'src/app/models/ad-type';
-
+import {AdTypeService} from '../../services/ad-type.service';
 @Component({
   selector: 'app-edit-ad-type',
   templateUrl: './edit-ad-type.component.html',
   styleUrls: ['./edit-ad-type.component.css'],
 })
 export class EditAdTypeComponent implements OnInit {
-  adTypeId: number;
-  adType: AdType;
-  constructor(private activeRouter: ActivatedRoute) {}
+ public  adTypeId: number;
+  public adType: AdType;
+  public type='Add';
+ 
+
+  constructor(private adTypeService: AdTypeService, 
+    private router: Router,
+    private route: ActivatedRoute ) { }
 
   ngOnInit(): void {
-    this.adType = new AdType();
-    this.adTypeId = this.activeRouter.snapshot.params['id'];
-  }
-  editType() {}
-}
+    this.route.paramMap.subscribe(params => {
+      console.log(params.get('id'));
+      if ( Number(params.get('id') ) != null) {
+      this.adTypeService.selected(params.get('id')).subscribe(
+        res => {
+          this.adType =  res as AdType; 
+        },
+        err => console.log(err)
+        );
+        this.adType = this.adTypeService.selectedAdType;
+        this.type = 'Edit'; 
+      }else {
+
+      this.adType = new AdType(0,'');
+    }
+  });
+ }
+
+ onAdd(): void {
+  if (this.type === 'Add') {
+    this.adTypeService.addPromotion(this.adType);
+    this.router.navigate(['/adType/add']);
+  } else {
+    console.log(this.adType);
+    this.adTypeService.updatePromotion(this.adType);
+    this.router.navigate(['/adType/list']);
+
+  }}}
+
